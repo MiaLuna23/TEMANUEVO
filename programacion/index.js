8c44fe1754f09a95d2b5603b7a2a1a916060622b
MiaLuna23 <166016904+MiaLuna23@users.noreply.github.com>
2024-07-25 08:18:13 -0300

    Add files via upload

---
+++ programacion/index.js
@@ -0,0 +1,27 @@
+/*
+Arrays - Arreglos
+|"bastian"| "matilda" | "santino"
+   0             1         2 
+ Coleccion de datos, ordenados por un indice.
+   
+   
+   
+   */
+  // Arreglo vacio
+let listaAlumnos = []
+let notaAlumnos = [10, 3, 5] // 3 arreglos
+0=10
+1=3
+2=5
+length=3
+// asignacion de valor con arreglos
+notaAlumnos [2]= 7
+
+//Agregar y eliminar elemntos del arreglo
+listaAlumnos.push("benja")//agrega elemntos al final
+listaAlumnos.pop()  //elimina el ultimo
+listaAlumnos.shift() // elimina el primero
+listaAlumnos.unshift("mia") //Agrega al principio
+//solicitar usuario 
+listaAlumnos.push(prompt("ingresa un nombre"))
+
